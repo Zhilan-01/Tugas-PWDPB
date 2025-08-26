9de7c5c91b0d5fc91dcb9f7eb04616d4cc964d3b
Zhilan-01 <zhilanmaulanaf@gmail.com>
2025-08-26 17:50:04 +0700

    Add files via upload

---
+++ pertemuan1PHP/hello.php
@@ -0,0 +1,6 @@
+<?php
+echo "ini adalah baris pertama<br>";
+echo "ini adalah baris kedua<br>";
+echo "Belajar PHP itu menyenangkan";
+echo ""
+?>
---
+++ pertemuan1PHP/latihan_2.php
@@ -0,0 +1,11 @@
+<?php
+//ini adalah komentar satu baris
+//*ini adalah komentar yang bisaa menjelaskan banyak baris*/
+/**
+ * fungsi ini menampilkan pesan
+ * @param string $messange
+ */
+function displayMessage($message){
+    echo $message;
+}
+?>
